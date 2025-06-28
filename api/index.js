6370998ef11376d1a8045b69c345e71d4e5aed06
kan327 <kannybaihaqi100@gmail.com>
2025-06-28 22:33:11 +0700

    debug

--- api/index.js
+++ api/index.js
@@ -66,11 +66,27 @@ app.get('/api/users', (req, res) => {
   }
 
   db.query(sql, values, (err, results) => {
-    if (err) return sendResponse(res, {
-      status: false,
-      message: 'Database error',
-      errorData: err
-    }, 500);
+    // if (err) return sendResponse(res, {
+    //   status: false,
+    //   message: 'Database error',
+    //   errorData: err
+    // }, 500);
+
+    if (err) {
+      return res.status(500).json({
+        status: false,
+        message: 'Database error',
+        errorMessage: err.message,
+        errorCode: err.code,
+        env: {
+          DB_HOST: process.env.DB_HOST,
+          DB_USER: process.env.DB_USER,
+          DB_PORT: process.env.DB_PORT,
+          DB_PASS: process.env.DB_PASS,
+          DB_NAME: process.env.DB_NAME
+        }
+      });
+    }
 
     sendResponse(res, {
       message: 'User list retrieved',
